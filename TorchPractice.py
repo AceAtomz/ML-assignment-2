dddaf10177055841760a83b7c337c3c60eca09aa
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-06-05 18:13:57 +0200

    torch working good

--- TorchPractice.py
+++ TorchPractice.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+torch.manual_seed(0)
 
 class Net(nn.Module):
     def __init__(self):
@@ -14,6 +15,7 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5*5 from image dimension
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
+        self.learning_rate = 0.01
 
     def forward(self, x):
         # Max pooling over a (2, 2) window
@@ -26,60 +28,33 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
-
 net = Net()
-#print(net)
 
-params = list(net.parameters())
-# print(len(params))
-# print(params[0].size())  # conv1's .weight
+params = list(net.parameters()) #10 params
+#print(params[1].size())  # conv1's .weight
+# for i in range(len(params)):
+#     if(i%2!=0):
+#         print(params[i])  # conv1's .weight
 
 input = torch.randn(1, 1, 32, 32)
-out = net(input)
-#print(out)
-
-net.zero_grad()
-out.backward(torch.randn(1, 10))
 
 output = net(input)
+print("first output", output)
 target = torch.randn(10)  # a dummy target, for example
 target = target.view(1, -1)  # make it the same shape as output
 criterion = nn.MSELoss()
 
-loss = criterion(output, target)
-# print(loss)
-# print(loss.grad_fn)  # MSELoss
-# print(loss.grad_fn.next_functions[0][0])  # Linear
-# print(loss.grad_fn.next_functions[0][0].next_functions[0][0])  # ReLU
-
-net.zero_grad()     # zeroes the gradient buffers of all parameters
-
-print('conv1.bias.grad before backward')
-print(net.conv1.bias.grad)
-
-# loss.backward()
-
-# print('conv1.bias.grad after backward')
-# print(net.conv1.bias.grad)
-
 import torch.optim as optim
-
 # create your optimizer
 optimizer = optim.SGD(net.parameters(), lr=0.01)
 
-learning_rate = 0.01
-for f in net.parameters():
-    f.data.sub_(f.grad.data * learning_rate)
-
-    # in your training loop:
+for i in range(100):
+     # in your training loop:
     optimizer.zero_grad()   # zero the gradient buffers
     output = net(input)
     loss = criterion(output, target)
     loss.backward()
     optimizer.step()    # Does the update
 
-    print('conv1.bias.grad after update')
-    print(net.conv1.bias.grad)
-
-
-
+print("final output", output)
+print("final loss", loss.item())
