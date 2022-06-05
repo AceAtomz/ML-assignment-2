8f7971bb62c31f7c69f2264274bbac73d5629cf4
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-06-05 18:53:04 +0200

    part 1 port torch into project

--- Assignment Training.py
+++ Assignment Training.py
@@ -1,16 +1,15 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
-import matplotlib.pyplot as plt
-import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 import sys
 from os import path
 
 inputFile = path.dirname(__file__)
 labelFile = path.dirname(__file__)
 
-
-class NeuralNet():
+class fileReader():
     def __init__(self):
         self.inputFile = open(path.join(inputFile, "./assignment_data/inputs.txt"),"r", encoding="utf-8")
         self.labelFile = open(path.join(labelFile, "./assignment_data/labels.txt"),"r", encoding="utf-8")
@@ -34,14 +33,10 @@ class NeuralNet():
     def toStringInputs(self):
         print(self.inputArr)
         print(self.labelArr)
-        # for i in self.inputArr:
-        #     print(self.inputArr[i])
-        #     print(self.labelArr[i])
 
-
+#----------------------------------------------------------------------------------------------------------
 #TODO: ins and outs
 #sys.stdout.write(str(num))
-
 def main():
     #load stdin
     np.loadtxt(sys.stdin)
@@ -52,12 +47,53 @@ def main():
         # Write stdout
         output = line
         sys.stdout.write(str(output))
-
 #main()
+#----------------------------------------------------------------------------------------------------------
 
-nn = NeuralNet()
-#nn.readFromFile()
-#nn.toStringInputs()
+#inputReader = fileReader()
+#inputReader.readFromFile()
+#inputReader.toStringInputs()
 
-import torch
-print(torch.cuda.is_available())
+torch.manual_seed(0)
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # 1 input image channel, 6 output channels, 5x5 square convolution
+        self.conv1 = nn.Linear(5, 6)
+        self.conv2 = nn.Linear(6, 6)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+        self.learning_rate = 0.01
+
+    def forward(self, x):
+        x = torch.flatten(x, 1) # flatten all dimensions except the batch dimension
+        #x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+params = list(net.parameters()) #10 params
+
+input = torch.randn(1, 1, 32, 32)
+output = net(input)
+print("first output", output)
+
+target = torch.randn(10)  # a dummy target, for example
+target = target.view(1, -1)  # make it the same shape as output
+criterion = nn.MSELoss()
+optimizer = optim.SGD(net.parameters(), lr=0.01)
+
+for i in range(10):
+     # in your training loop:
+    optimizer.zero_grad()   # zero the gradient buffers
+    output = net(input)
+    loss = criterion(output, target)
+    loss.backward()
+    optimizer.step()    # Does the update
+
+print("final output", output)
+print("final loss", loss.item())
