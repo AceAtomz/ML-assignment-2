20d5a3f3e1d5df29de164ffab17a25c8cbc423d7
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-06-06 14:44:24 +0200

    single input condition

--- run.py
+++ run.py
@@ -1,33 +1,37 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import numpy as np
 import sys
 
-PATH = "state_dict_model.pt"
-
-class Net(nn.Module):
+class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(2352, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1 = torch.nn.Linear(2352, 120)
+        self.fc2 = torch.nn.Linear(120, 84)
+        self.fc3 = torch.nn.Linear(84, 10)
 
     def forward(self, x):
         x = torch.flatten(x, 1) # flatten all dimensions except the batch dimension
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = torch.nn.functional.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
 def main():
+    PATH = "state_dict_model.pt"
     model = Net()
     model.load_state_dict(torch.load(PATH))
     model.eval()
-    test_data = np.loadtxt(sys.stdin)
 
-    for data_point in test_data:
-        input = torch.tensor(np.array([data_point], dtype=np.float32))
+    test_data = np.loadtxt(sys.stdin)
+    shape = test_data.shape
+    if(shape != (2352,)):
+        for data_point in test_data:
+            input = torch.tensor(np.array([data_point], dtype=np.float32))
+            output = model(input)
+            prediction = torch.argmax(output)
+            sys.stdout.write(str(prediction.item()))
+    else:
+        input = torch.tensor(np.array([test_data], dtype=np.float32))
         output = model(input)
         prediction = torch.argmax(output)
         sys.stdout.write(str(prediction.item()))
